598d514e287b830c8a6e495351dc022e4df13453
Aaron Swartz <me@aaronsw.com>
2011-02-05 23:36:54 -0500

    first night's work

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 import web
+
 web.template.Template.globals['datestr'] = web.datestr
+web.template.Template.globals['len'] = len
+
 db = web.database(dbn='postgres', db='rtask', user='postgres', pw='x')
 render = web.template.render('templates/', cache=False)
+now = web.SQLLiteral('now()')
+
 urls = (
   '/', 'main', 
-  '/edit_name', 'edit_name'
+  '/add', 'add',
+  '/edit', 'edit',
+  '/reset', 'reset'
 )
 
 class main:
 	def GET(self):
-		r = db.select('tasks')
-		return render.index(r[0])
+		q = "and completed is null and (postponed_until is null or postponed_until < now()) and someday is null"
+		focus = db.select('tasks', where="stalled='f' " + q)
+		stalled = db.select('tasks', where="stalled='t' " + q)
+		return render.index(focus, stalled)
 
-class edit_name:
+class add:
 	def POST(self):
-		i = web.input('id', 'name')
-		db.update('tasks', where="id=$id", name=i.name, vars=i)
+		i = web.input('name')
+		db.insert('tasks', name=i.name)
+		raise web.seeother('/')
+
+class edit:
+	def POST(self):
+		i = web.input('id')
+		out = web.storage()
+		if i.get('name'):
+			out.name = i.name
+		elif i.get('stall'):
+			out.stalled=True
+		elif i.get('finished'):
+			out.completed = now 
+			out.gave_up = (i.finished=='gaveup')
+		elif i.get('postpone'):
+			if i.postpone == 'someday':
+				out.someday = True
+			else:
+				out.postponed_until = web.SQLLiteral('now() + ' + web.sqlquote(i.postpone)) 
+		db.update('tasks', where="id=$id", vars=i, **out)
+		raise web.seeother('/')
+
+class reset:
+	def POST(self):
+		db.update('tasks', where="stalled='t'", stalled=False)
 		raise web.seeother('/')
 
 app = web.application(urls, globals())
--- schema.sql
+++ schema.sql
@@ -13,8 +13,13 @@ CREATE TABLE tasks (
 	id serial primary key,
 	user_id int default 1,
 	created timestamp default 'now',
+	completed timestamp,
+	postponed_until date,
+	someday boolean,
 	name text,
-	project_id int references projects
+	project_id int references projects,
+	stalled boolean default 'f',
+	gave_up boolean
 );
 
 insert into projects (name) values ('rtask');
